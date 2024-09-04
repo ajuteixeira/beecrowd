d16e714973a5e6dfa9101c544e1a393efbc98281
ajuteixeira <teixeirajuuh@gmail.com>
2024-09-03 21:23:31 -0300

    resolução 1009

--- 1009.js
+++ 1009.js
@@ -1,16 +1,15 @@
 // BEE1009
 
 // Entrada de valores
-const input = require('fs').readFileSync('data/1009.txt', 'utf8');
-const lines = input.split('\r\n');
+const input = require("fs").readFileSync("data/1009.txt", "utf8");
+const lines = input.split("\r\n");
 
 // Declaração de variáveis
-let nomeVend = lines[0];
 let salFixo = parseFloat(lines[1]);
 let totVendas = parseFloat(lines[2]);
 
 // Calculando o valor total a receber
-let totReceber = salFixo + (totVendas * 0,15)
+let totReceber = salFixo + (totVendas * 0.15);
 
 // Saída de resultados
-console.log(`TOTAL = R$ ${totReceber}`)
+console.log(`TOTAL = R$ ${totReceber.toFixed(2)}`);
