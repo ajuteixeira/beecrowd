6a04a28fdcd7fb32cf606526bc26a8fb742dd79a
ajuteixeira <teixeirajuuh@gmail.com>
2024-09-19 15:31:31 -0300

    1041

--- 1041.js
+++ 1041.js
@@ -5,9 +5,8 @@ const input = require('fs').readFileSync('data/1041.txt', 'utf8');
 const lines = input.split('\r\n');
 
 // Declarando variáveis
-let line = lines[2].split(' ');
-let x = parseFloat(line[0]);
-let y = parseFloat(line[1]);
+let x = parseFloat(lines[0]);
+let y = parseFloat(lines[1]);
 
 // Construindo a estrutura condicional
 if (y > 0 && x > 0) {
@@ -21,7 +20,7 @@ if (y > 0 && x > 0) {
 } else if (y == 0 && x == 0) {
   console.log('Origem');
 } else if (x == 0) {
-  console.log('Eixo X');
-} else if (y == 0) {
   console.log('Eixo Y');
+} else if (y == 0) {
+  console.log('Eixo X');
 }
